c801a2dc17b7ddb8d809f613ef265e45abd3fe46
Suhas H Acharya <suhashacharya@Suhass-MacBook-Air.local>
2024-07-10 02:03:34 -0700

    meme create v1

--- app/page.js
+++ app/page.js
@@ -1,113 +1,181 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect, useRef } from "react";
+import "./globals.css";
+import Sidebar from "./Sidebar";
+import { Rnd } from "react-rnd";
+import html2canvas from "html2canvas";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+export default function App() {
+  const [memes, setMemes] = useState([]);
+  const [currentMeme, setMeme] = useState(null);
+  const [memeText, setMemeText] = useState([]);
+  const memeImage = useRef(null);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setMemes(data.data.memes));
+
+    const memeTextArr = [];
+    for (let i = 0; i < 6; i++) {
+      memeTextArr.push({ body: "", size: 16 });
+    }
+
+    setMemeText(memeTextArr);
+  }, []);
+
+  function updateCurrentMeme(id) {
+    const newmeme = memes.find((meme) => meme.id === id);
+    setMeme(newmeme);
+  }
+
+  function updateMemeText(event, index, prop) {
+    const newVal =
+      prop === "size" ? parseInt(event.target.value, 10) : event.target.value;
+
+    setMemeText((prev) => {
+      const newText = [...prev];
+      newText[index][prop] = newVal;
+      return newText;
+    });
+  }
+
+  function downloadMeme() {
+    if (memeImage.current)
+      html2canvas(memeImage.current, { useCORS: true }).then((canvas) => {
+        let a = document.createElement("a");
+        // document.body.appendChild(a);
+        a.download = `${currentMeme.name}.png`;
+        a.href = canvas.toDataURL();
+        a.click();
+      });
+    // console.log(currentMeme , " was downloaded");
+  }
+
+  /*
+  +===================
+  \\
+  \\ size value is incrementing and Decrementing by 2, Don't know why
+  \\
+  +===================
+  */
+
+  function sizePlus(index) {
+    console.log("sizePlus at", index);
+    setMemeText((prev) => {
+      const newArr = [...prev];
+      newArr[index].size = prev[index].size + 1;
+      return newArr;
+    });
+  }
+
+  function sizeMinus(index) {
+    console.log("sizeMinus at", index);
+    setMemeText((prev) => {
+      const newArr = [...prev];
+      newArr[index].size = prev[index].size - 1;
+      return newArr;
+    });
+  }
+
+  const renderThumbnail = memes.map((meme) => {
+    return (
+      <img
+        key={meme.id}
+        className="thumbnail"
+        onClick={() => updateCurrentMeme(meme.id)}
+        src={meme.url}
+        alt="meme thumbnail"
+      />
+    );
+  });
+
+  const renderInputs = () => {
+    const retArr = [];
+    // const memeTextArr = [];
+    const boxes = (currentMeme && currentMeme.box_count) || 2;
+    // console.log("box count", boxes);
+    for (let i = 0; i < boxes; i++) {
+      retArr.push(
+        <div key={i} className="input-container">
+          <input
+            type="text"
+            value={(memeText[i] && memeText[i].body) || ""}
+            className="memeInput"
+            onChange={(event) => updateMemeText(event, i, "body")}
+            placeholder={`text ${i + 1}`}
+          />
+          <div className="size-container">
+            <button className="sizeBut" onClick={() => sizeMinus(i)}>
+              -
+            </button>
+            <input
+              type="text"
+              className="sizeInput"
+              maxLength="3"
+              onChange={(e) => updateMemeText(e, i, "size")}
+              value={(memeText[i] && memeText[i].size) || 16}
             />
-          </a>
+            <button className="sizeBut" onClick={() => sizePlus(i)}>
+              +
+            </button>
+          </div>
         </div>
-      </div>
+      );
+    }
+    return retArr;
+  };
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+  const renderMemeText = () => {
+    const retArr = [];
+    const boxes = (currentMeme && currentMeme.box_count) || 2;
+    // console.log("box count", boxes);
+    for (let i = 0; i < boxes; i++) {
+      retArr.push(
+        <Rnd
+          key={i}
+          className="meme-text"
+          bounds=".text-container"
+          style={{ fontSize: (memeText[i] && memeText[i].size) || 16 }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          {memeText[i].body}
+        </Rnd>
+      );
+    }
+    return retArr;
+  };
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+  return (
+    <div className="App">
+      <header className="header">
+        <span className="fa-solid fa-dice"></span> Meme Generator
+      </header>
+      <main>
+        <Sidebar>
+          {memes.length ? renderThumbnail : <h2>Loading...</h2>}
+        </Sidebar>
+        <div className="img-container">
+          {currentMeme ? (
+            <div ref={memeImage} className="text-container">
+              {renderMemeText()}
+              <img
+                className="current-meme outline"
+                src={currentMeme.url}
+                alt="current meme"
+              />
+            </div>
+          ) : (
+            <h2> Select Meme On Left to Get started </h2>
+          )}
+        </div>
+        <div className="inputs">
+          {renderInputs()}
+          <button className="download" onClick={downloadMeme}>
+            {" "}
+            Download 🖼{" "}
+          </button>
+        </div>
+      </main>
+    </div>
   );
 }
